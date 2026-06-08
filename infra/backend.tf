96dc5cbbecff44c0e9ffe2282596cb62b4186df4
Axel <axel.maral@epita.fr>
2026-06-08 18:34:18 +0200

    fix: update all region references from eu-west-1 to us-east-1

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "pv-tf-state"
     key            = "terraform.tfstate"
-    region         = "eu-west-1"
+    region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "pv-tf-state-lock"
   }
--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -9,5 +9,5 @@ variable "vpc_cidr" {
 
 variable "azs" {
   type    = list(string)
-  default = ["eu-west-1a", "eu-west-1b"]
+  default = ["us-east-1a", "us-east-1b"]
 }
